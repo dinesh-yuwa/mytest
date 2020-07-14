678a850bfd5af13114c01003ed5c6c1c4324e13a
dinesh-yuwa <yuwarajalingam@gmail.com>
2020-07-14 12:51:20 +0530

    header add and shoppage updated

--- src/component/colection-item/colection-item.component.jsx
+++ src/component/colection-item/colection-item.component.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './colection-item.style.scss';
 
-const ColectionItem=( {id,name,price,imageUrl}) =>(
+const ColectionItem=( {name,price,imageUrl}) =>(
     <div className='colection-item'>
-        <div className='colection-image' style={{backgroundImage:`url(${imageUrl})`}}></div>
-        <div className='colection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
-
-    </div>
+        <div className='colection-image' style={{backgroundImage:`url(${imageUrl})`}}>
+            <div className='colection-footer'>           
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            
+        </div>   
+          
+    </div>  
+            
+        
+    
+    
 
 
 
